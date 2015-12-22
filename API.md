198fffbc23c07ed7ed59059df810bc9468bcf536
danigb <danigb@gmail.com>
2015-12-22 11:56:46 +0100

    Split code into two classes: Sample and Player. Coverage report.

--- API.md
+++ API.md
@@ -1,15 +1,50 @@
 ## `Player`
 
-Create a audio buffer player
+Create a audio buffer player. An audio buffer player allows to play audio
+buffers easily.
+
+It has three methods: `start`, `stop` and `connect`
 
 ### Parameters
 
-* `destination` **`AudioDestinationNode or AudioContext`** the destination or the audio context. In the first case, the player will connect to that destination. In the second, you will have to call `connect` explicitly
-* `options` **`Hash`** the sample play configuration  Valid options:  - gain: the audio gain (default: 1) - loop: loop or not the audio (default: false) - filter: the filter type (default: 'highpass') - freq: the filter frequency (default: 0)
+* `destination` **`AudioNode or AudioContext`** the destination or the audio context. In the first case, the player will connect to that destination. In the second, you will have to call `connect` explicitly
+* `options` **`Hash`** (Optional) the default sample configuration. See `Sample` constructor
 
 
+### Examples
+
+```js
+var player = new Player(ac.destination)
+// play the same buffer with 1 second of difference
+player.start(buffer, ac.currentTime)
+player.start(buffer, ac.currentTime + 1)
+```
 
 Returns `Player` the sample player
+
+
+## `player.connect`
+
+Set player destination
+
+If you pass an audio destination in the Player constructor, you won't need
+to call this method
+
+### Parameters
+
+* `destination` **`AudioNode`** the sample destination
+
+
+### Examples
+
+```js
+var player = new Player(ac)
+player.connect(ac.destination)
+// same as:
+var player = new Player(ac.destination)
+```
+
+Returns `Player` the player (chainable function)
 
 
 ## `player.start`
@@ -25,13 +60,123 @@ Start a buffer
 * `destination` **`AudioDestinationNode`** (Optional) a destination that overrides the default routing
 
 
+### Examples
+
+```js
+var player = new Player(ac.destination)
+player.start(buffer, ac.currentTime, 1, { loop: true })
+```
 
 Returns `Object` an object with the following connected nodes:
+
+
+## `player.stop`
+
+Stop some or all of the playing samples
+
+### Parameters
+
+* `when` **`Float`** the time to schedule the stop
+* `ids` **`Integer or Array<Integer>`** (Optional) the ids of the samples to stop or stop all samples if no value is provided
+
+
+### Examples
+
+```js
+var player = new Player(ac.destination, { loop: true })
+player.start(drumLoop1)
+player.start(drumLoop2)
+player.start(drumLoop3)
+player.stop(ac.currentTime + 10) // stop all loops after 10 seconds
+```
+
+
+
+## `Sample`
+
+Create an audio sample. An audio sample is an object with the following
+connected nodes:
 
 - source: a buffer source node
 - filter: a biquad filter node
 - env: a gain envelop adsr node
 - amp: a gain node
-- stop: a function to stop the sound at the given time
+
+They are public, and you can modify them before play.
+
+Additionally, an audio sample has a `start` and  `stop` methods and a `onended`
+event handler.
+
+### Parameters
+
+* `buffer` **`AudioBuffer`** (Required) the audio buffer
+* `destination` **`AudioNode`** (Required) the audio destination
+* `options` **`Hash or Function`** (Optional) the sample options
+* `fire` **`Function`** (Optiona) a function to fire events
+
+
+### Examples
+
+```js
+var sample = new Sample(buffer, ac.destination)
+sample.play(ac.currentTime)
+```
+
+Returns `Sample` the sample
+
+
+## `sample.onended`
+
+A event handler that contains the callback associated with the `ended`
+audio buffer source event
+
+
+
+### Examples
+
+```js
+var sample = new Sample(buffer, ac.destination)
+sample.onended = function(sample) { console.log('ended!') }
+```
+
+
+
+## `sample.start`
+
+Schedule the sample to start. Only can be called once.
+
+### Parameters
+
+* `when` **`Float`** (Optional) when to start the sample or now
+* `duration` **`Float`** (Optional) the duration in seconds. Only if its greater than 0, the `stop` method of sample will be called after that duration
+
+
+### Examples
+
+```js
+var sample = new Sample(buffer, ac.destination)
+sample.start()
+sample.start() // => thows Error. Create a new sample instead.
+```
+
+
+
+## `sample.stop`
+
+Schedule the sample to stop.
+
+### Parameters
+
+* `when` **`Float`** (Optiona) when to stop the sample, or now
+
+
+### Examples
+
+```js
+var sample = new Sample(buffer, ac.destination)
+sample.start(ac.currentTime + 1)
+sample.stop(ac.currentTime + 2)
+```
+
 
 
--- index.js
+++ index.js
@@ -2,17 +2,7 @@
 
 var ADSR = require('adsr')
 
-/**
- * Create an ADSR envelope with the adsr options
- * @private
- */
-function adsr (ac, adsr) {
-  return ['attack', 'decay', 'sustain', 'release'].reduce(function (env, n, i) {
-    env[n] = adsr[i]
-    return env
-  }, ADSR(ac))
-}
-
+// Default sample options
 var DEFAULTS = {
   gain: 1,
   loop: false,
@@ -25,29 +15,143 @@ var DEFAULTS = {
 }
 
 /**
- * Create a audio buffer player
+ * Create an audio sample. An audio sample is an object with the following
+ * connected nodes:
+ *
+ * - source: a buffer source node
+ * - filter: a biquad filter node
+ * - env: a gain envelop adsr node
+ * - amp: a gain node
+ *
+ * They are public, and you can modify them before play.
+ *
+ * Additionally, an audio sample has a `start` and  `stop` methods and a `onended`
+ * event handler.
+ *
+ * @param {AudioBuffer} buffer - (Required) the audio buffer
+ * @param {AudioNode} destination - (Required) the audio destination
+ * @param {Hash|Function} options - (Optional) the sample options
+ * @param {Function} fire - (Optiona) a function to fire events
+ * @return {Sample} the sample
+ *
+ * @example
+ * var sample = new Sample(buffer, ac.destination)
+ * sample.play(ac.currentTime)
+ */
+function Sample (buffer, destination, options, event) {
+  if (!(this instanceof Sample)) return new Sample(buffer, destination, options, event)
+  var s = this
+  var opts = typeof options === 'function' ? options : from(options, {}, {})
+  s.ac = destination.context
+  s.event = event || function () {}
+  s.amp = s.ac.createGain()
+  s.amp.gain.value = 0
+  s.amp.connect(destination || s.output)
+  s.env = adsr(s.ac, opts('adsr'))
+  s.env.value.value = opts('gain')
+  s.env.connect(s.amp.gain)
+
+  s.filter = s.ac.createBiquadFilter()
+  s.filter.type = opts('filter')
+  s.filter.frequency.value = opts('freq')
+  s.filter.connect(s.amp)
+
+  s.source = s.ac.createBufferSource()
+  s.source.buffer = buffer
+  s.source.loop = opts('loop')
+  s.source.connect(s.filter)
+  s.source.playbackRate.value = centsToRate(opts('pitch') * 100)
+  s.source.loopStart = opts('loopStart')
+  s.source.loopEnd = opts('loopEnd')
+
+  s.source.onended = function () {
+    s.event('ended', s)
+    s.source.stop()
+    s.source.disconnect()
+    s.filter.disconnect()
+    s.amp.disconnect()
+    s.env.disconnect()
+    s.onended(this)
+  }
+}
+
+/**
+ * A event handler that contains the callback associated with the `ended`
+ * audio buffer source event
+ *
+ * @name sample.onended
+ * @example
+ * var sample = new Sample(buffer, ac.destination)
+ * sample.onended = function(sample) { console.log('ended!') }
+ */
+Sample.prototype.onended = function () { }
+
+/**
+ * Schedule the sample to start. Only can be called once.
+ *
+ * @name sample.start
+ * @function
+ * @param {Float} when - (Optional) when to start the sample or now
+ * @param {Float} duration - (Optional) the duration in seconds. Only if its
+ * greater than 0, the `stop` method of sample will be called after that duration
+ *
+ * @example
+ * var sample = new Sample(buffer, ac.destination)
+ * sample.start()
+ * sample.start() // => thows Error. Create a new sample instead.
+ */
+Sample.prototype.start = function (when, duration) {
+  this.event('start', this, when)
+  this.env.start(when)
+  this.source.start(when)
+  if (duration > 0) this.stop(when + duration)
+}
+
+/**
+ * Schedule the sample to stop.
+ *
+ * @name sample.stop
+ * @function
+ * @param {Float} when - (Optiona) when to stop the sample, or now
+ *
+ * @example
+ * var sample = new Sample(buffer, ac.destination)
+ * sample.start(ac.currentTime + 1)
+ * sample.stop(ac.currentTime + 2)
+ */
+Sample.prototype.stop = function (when) {
+  this.event('stop', this, when)
+  var stopAt = this.env.stop(when || this.ac.currentTime)
+  this.source.stop(stopAt)
+}
+
+/**
+ * Create a audio buffer player. An audio buffer player allows to play audio
+ * buffers easily.
+ *
+ * It has three methods: `start`, `stop` and `connect`
  *
  * @name Player
  * @function
- * @param {AudioDestinationNode|AudioContext} destination - the destination
+ * @param {AudioNode|AudioContext} destination - the destination
  * or the audio context. In the first case, the player will connect to that
  * destination. In the second, you will have to call `connect` explicitly
- * @param {Hash} options - the sample play configuration
- *
- * Valid options:
- *
- * - gain: the audio gain (default: 1)
- * - loop: loop or not the audio (default: false)
- * - filter: the filter type (default: 'highpass')
- * - freq: the filter frequency (default: 0)
- *
+ * @param {Hash} options - (Optional) the default sample configuration.
+ * See `Sample` constructor
  * @return {Player} the sample player
+ * @see Sample
+ *
+ * @example
+ * var player = new Player(ac.destination)
+ * // play the same buffer with 1 second of difference
+ * player.start(buffer, ac.currentTime)
+ * player.start(buffer, ac.currentTime + 1)
  */
 function Player (ac, options) {
   if (!(this instanceof Player)) return new Player(ac, options)
 
   this.options = options || {}
-  this._id = 0
+  this.nextId = 1
   this._playing = {}
   this.onevent = function () {}
 
@@ -61,7 +165,6 @@ function Player (ac, options) {
     this.output = ac.createGain()
   }
 }
-var player = Player.prototype
 
 /**
  * Start a buffer
@@ -76,64 +179,24 @@ var player = Player.prototype
  * overrides the default routing
  * @return {Object} an object with the following connected nodes:
  *
- * - source: a buffer source node
- * - filter: a biquad filter node
- * - env: a gain envelop adsr node
- * - amp: a gain node
- * - stop: a function to stop the sound at the given time
+ * @example
+ * var player = new Player(ac.destination)
+ * player.start(buffer, ac.currentTime, 1, { loop: true })
  */
-player.start = function (buffer, when, duration, options, destination) {
+Player.prototype.start = function (buffer, when, duration, options, destination) {
   var ac = this.ac
   var event = this.onevent
   var tracked = this._playing
   when = when || ac.currentTime
 
-  var p = { id: ++this._id }
   var opts = from(options || {}, this.options, DEFAULTS)
 
-  p.amp = ac.createGain()
-  p.amp.gain.value = 0
-  p.amp.connect(destination || this.output)
-  p.env = adsr(ac, opts('adsr'))
-  p.env.value.value = opts('gain')
-  p.env.connect(p.amp.gain)
-
-  p.filter = ac.createBiquadFilter()
-  p.filter.type = opts('filter')
-  p.filter.frequency.value = opts('freq')
-  p.filter.connect(p.amp)
-
-  p.source = ac.createBufferSource()
-  p.source.buffer = buffer
-  p.source.loop = opts('loop')
-  p.source.connect(p.filter)
-  p.source.playbackRate.value = centsToRate(opts('pitch') * 100)
-  p.source.loopStart = opts('loopStart')
-  p.source.loopEnd = opts('loopEnd')
-
-  p.stop = function (when) {
-    event('stop', p, when)
-    var stopAt = p.env.stop(when || ac.currentTime)
-    p.source.stop(stopAt)
-  }
-
-  tracked[p.id] = p
-  p.source.onended = function () {
-    event('ended', p)
-    p.source.stop()
-    p.source.disconnect()
-    p.filter.disconnect()
-    p.amp.disconnect()
-    p.env.disconnect()
-    delete tracked[p.id]
-  }
-
-  event('start', p, when)
-  p.env.start(when)
-  p.source.start(when)
-  if (duration > 0) p.stop(when + duration)
-
-  return p
+  var s = new Sample(buffer, destination || this.output, opts, event)
+  s.id = this.nextId++
+  tracked[s.id] = s
+  s.onended = function () { delete tracked[s.id] }
+  s.start(when, duration)
+  return s
 }
 
 /**
@@ -142,9 +205,17 @@ player.start = function (buffer, when, duration, options, destination) {
  * @name player.stop
  * @function
  * @param {Float} when - the time to schedule the stop
- * @param {Integer|Array<Integer>} ids - (Optional) the ids of the samples to stop
+ * @param {Integer|Array<Integer>} ids - (Optional) the ids of the samples to
+ * stop or stop all samples if no value is provided
+ *
+ * @example
+ * var player = new Player(ac.destination, { loop: true })
+ * player.start(drumLoop1)
+ * player.start(drumLoop2)
+ * player.start(drumLoop3)
+ * player.stop(ac.currentTime + 10) // stop all loops after 10 seconds
  */
-player.stop = function (when, ids) {
+Player.prototype.stop = function (when, ids) {
   when = when || 0
   var tracked = this._playing
 
@@ -164,14 +235,36 @@ player.stop = function (when, ids) {
 /**
  * Set player destination
  *
+ * If you pass an audio destination in the Player constructor, you won't need
+ * to call this method
+ *
+ * @name player.connect
+ * @function
  * @param {AudioNode} destination - the sample destination
  * @return {Player} the player (chainable function)
+ *
+ * @example
+ * var player = new Player(ac)
+ * player.connect(ac.destination)
+ * // same as:
+ * var player = new Player(ac.destination)
  */
-player.connect = function (destination) {
+Player.prototype.connect = function (destination) {
   this.output.connect(destination)
   return this
 }
 
+Player.Sample = Sample
+
+if (typeof module === 'object' && module.exports) module.exports = Player
+if (typeof window !== 'undefined') window.Player = Player
+
+// /////////////////////////////// PRIVATE  /////////////////////////////// //
+
+/**
+ * Create a hash accessor
+ * @private
+ */
 function from (a, b, c) {
   return function (name) {
     return name in a ? a[name] : (name in b ? b[name] : c[name])
@@ -187,5 +280,13 @@ function from (a, b, c) {
  */
 function centsToRate (cents) { return Math.pow(2, cents / 1200) }
 
-if (typeof module === 'object' && module.exports) module.exports = Player
-if (typeof window !== 'undefined') window.Player = Player
+/**
+ * Create an ADSR envelope with the adsr options
+ * @private
+ */
+function adsr (ac, adsr) {
+  return ['attack', 'decay', 'sustain', 'release'].reduce(function (env, n, i) {
+    env[n] = adsr[i]
+    return env
+  }, ADSR(ac))
+}
