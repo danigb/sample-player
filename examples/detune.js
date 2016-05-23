cb0e93db133e0e883d6bd219a75a1ea818b1cee7
danigb <danigb@gmail.com>
2016-05-23 05:30:43 +0200

    Examples

--- examples/detune.js
+++ examples/detune.js
@@ -4,10 +4,11 @@ var player = require('..')
 var ac = new AudioContext()
 
 document.body.innerHTML = '<h1>Detune example (sample-player)</h1>(open the dev console)'
-console.log('Loading...')
+console.log('Loading snare')
 load(ac, 'examples/audio/snare.wav').then(function (buffer) {
-  var snare = player(ac, buffer)
-  snare.events.repeat(10, function (i) {
-    return { time: i * 0.5 }
+  var snare = player(ac, buffer).connect(ac.destination)
+  console.log('loaded')
+  snare.schedule(24, function (i) {
+    return { time: i * 0.3, cents: 400 * i - 2400 }
   })
 })
---
+++ examples/loop.js
@@ -0,0 +1,18 @@
+/* global AudioContext */
+var load = require('audio-loader')
+var player = require('..')
+var ac = new AudioContext()
+
+document.body.innerHTML = '<h1>Loop example (sample-player)</h1>(open the dev console)'
+console.log('Loading sample...')
+load(ac, 'examples/audio/440Hz.mp3').then(function (buffer) {
+  console.log('loaded')
+  var sine = player(ac, buffer, { gain: 0.3, loop: true, adsr: [1, 0.5, 0.5, 5] })
+  sine.onevent = function (a, b, c) { console.log(a, b, c) }
+  sine.connect(ac.destination)
+  sine.start()
+  setTimeout(function () {
+    console.log('stop player')
+    sine.stop()
+  }, 8000)
+})
