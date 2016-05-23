54c1eac479f83495ef838c1c134c909ac2439046
danigb <danigb@gmail.com>
2016-05-23 11:32:26 +0200

    Add config files. Start tests

--- test/sample-player-test.js
+++ test/sample-player-test.js
@@ -1,37 +1,17 @@
 /* globals describe it AudioContext */
 require('web-audio-test-api')
 var assert = require('assert')
-var ac = new AudioContext()
 var Player = require('..')
-var sinon = require('sinon')
+// var sinon = require('sinon')
 
-function audio (ch, secs) { return ac.createBuffer((ch || 1), (secs || 1) * ac.sampleRate, ac.sampleRate) }
+function buffer (ac) { return ac.createBuffer(2, 22050, 44100) }
 
-describe('sample-player', () => {
-  it('creates player from destination', () => {
-    var player = Player(ac.destination)
-    assert.equal(player.ac, ac)
-  })
-
-  it('have options', function () {
-    var player = Player(ac)
-    assert.deepEqual(player.options, {})
-  })
-
-  it('plays', function () {
-    var player = Player(ac)
-    var sample = player.start(audio())
-    assert.equal(typeof sample.stop, 'function')
-  })
-
-  describe('events', function () {
-    it('fires start event', function () {
-      var player = Player(ac.destination)
-      var events = player.onEvent = sinon.spy()
-      player.start(audio())
-      assert.equal(events.callCount, 1)
-      assert.equal(events.getCall(0).args[0], 'start')
-      assert.equal(events.getCall(0).args[1].id, 1)
+describe('sample-player', function () {
+  describe('SamplePlayer', function () {
+    it('creates player a player', () => {
+      var ac = new AudioContext()
+      var player = Player(ac, buffer(ac))
+      assert(player)
     })
   })
 })
