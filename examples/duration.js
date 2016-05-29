421d9b3a48ecc1d093b1dca7a3d0bf278070ddc0
danigb <danigb@gmail.com>
2016-05-29 23:37:28 +0200

    Add duration example

---
+++ examples/duration.js
@@ -0,0 +1,20 @@
+/* global AudioContext */
+var load = require('audio-loader')
+var player = require('..')
+var ac = new AudioContext()
+var NOTES = 'C4 D4 E4 F4 G4 A4 B4 C5'.split(' ')
+
+document.body.innerHTML = '<h1>Duration (sample-player example)</h1>(open the dev console)'
+console.log('Loading sample...')
+load(ac, 'examples/audio/piano.js').then(function (buffers) {
+  console.log('loaded')
+  var piano = player(ac, buffers, { release: 1 }).connect(ac.destination)
+  piano.on('start', function (a, b, c) {
+    console.log(a, b, c)
+  })
+  var notes = NOTES.concat(NOTES.slice(0, -1).reverse())
+  console.log('schedule', notes)
+  piano.schedule(notes, function (note, i) {
+    return { name: note, time: 0.2 * i, duration: 0.05 }
+  })
+})
